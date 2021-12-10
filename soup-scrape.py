779053e53be8585e2a2acf6c6c991ae5c8bc265e
Vanessa Trujillo <vanessa-trujiilo@live.com>
2021-12-09 18:16:36 -0800

    scrape sample wiki page

--- soup-scrape.py
+++ soup-scrape.py
@@ -1,8 +1,9 @@
 from bs4 import BeautifulSoup
 import requests
-import pandas as pd
 #Target web page
-url = "https://projects.fivethirtyeight.com/trump-approval-ratings/"
+
+wikiSearch = "Genghis_Khan"
+url = "https://en.wikipedia.org/wiki/" + wikiSearch
 
 #Connection to web page
 response = requests.get(url)
@@ -11,15 +12,15 @@ print(response.status_code)
 # Convert the response HTLM string into a python string
 html = response.text
 
-soup = BeautifulSoup(response.content, 'lxml')
+soup = BeautifulSoup(html, 'lxml')
 
 #List to store results
 textContent = []
 
 #Get all headers from the latest section of the web site
-for i in range(0, 4):
+for i in range(0, 10):
     the_latest = soup.find_all("h3")[i].text
     textContent.append(the_latest)
 
 print(textContent)
-pd.DataFrame(textContent)
+#pd.DataFrame(textContent)
